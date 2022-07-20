fd2c7fcafd0cbe9342d8bb7f4510234f6fc00986
fasilfaisy1024 <42532766+fasilfaisy1024@users.noreply.github.com>
2022-07-20 21:03:34 +0530

    Update sumof3digt.sh

--- sumof3digt.sh
+++ sumof3digt.sh
@@ -3,11 +3,19 @@ read num
 
 sum=0
 
-while [ $num -gt 0 ]
+while [ $num -gt 9 ]
 do
+   while [ $num -gt 0 ]
+   do
     mod=$((num % 10))    #It will split each digits
     sum=$((sum + mod))   #Add each digit to sum
     num=$((num / 10))    #divide num by 10.
+done
+   if [ $sum -gt 9 ]
+   then
+   num=$sum
+   sum=0
+   fi
 done
 
 echo $sum
